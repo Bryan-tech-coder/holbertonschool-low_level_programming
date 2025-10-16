895ca4fd2cd60b2a7bdac36bc143e3e17e1a6442
bryan <br1173626@gmail.com>
2025-10-15 21:09:12 -0400

    ch

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Imprime el alfabeto en minúsculas seguido de nueva línea
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
+
