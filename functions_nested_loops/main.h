f246319bbb6374b1f6728dee93564741656da2a2
bryan <br1173626@gmail.com>
2025-10-15 20:25:52 -0400

    ch

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,19 +1,17 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
+void print_ alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_ last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 int _putchar(char c);
-
-void print_ alphabet (void) ;
-void print_alphabet_x10 (void) ;
-int _islower (int c) ;
-int _isalpha (int c) ;
-int print_ sign(int n);
-int_abs (int) ;
-int print_ last_digit (int) ;
-void jack_bauer (void) ;
-void times_table (void) ;
-int add (int, int);
-void print_to_98 (int n) ;
-
-#endif 
+#endif
 
