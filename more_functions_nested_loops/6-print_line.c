b4f6d3373dd1189ae2a6fcbbc208ad65c9d491d3
bryan <br1173626@gmail.com>
2025-10-20 12:01:03 -0400

    li

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  * print_line - draws a straight line in the terminal
- * number of times the character '_' should be printed
+ * @n: number of times the character '_' should be printed
  *
  * Return: void
  */
