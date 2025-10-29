810734c69c89868254c8ecd45ab3f2b3487ee3d8
Bryan Ramos <br1173626@gmail.com>
2025-10-29 10:40:17 -0400

    prints a string in reverse

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: pointer to the string to print
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
+}
