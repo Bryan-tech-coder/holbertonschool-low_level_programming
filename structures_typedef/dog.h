35ea6e86ca91a68c8a4107b3187f4a05a3ae5a75
Bryan Ramos <br1173626@gmail.com>
2025-11-05 13:16:54 -0400

    define a new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * struct dog - structure that stores information about a dog
+ * @name: pointer to the dog's name (string)
+ * @age: dog's age (float)
+ * @owner: pointer to the owner's name (string)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
