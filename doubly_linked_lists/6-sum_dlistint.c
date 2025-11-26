5cec02bf4b581b8855ec05ae9eddd9ce222ee991
Bryan <br1173626@gmail.com>
2025-11-26 13:04:25 -0400

    l

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -2,7 +2,7 @@
 
 
 /**
- * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list
+ * sum_dlistint - returns the sum of all the data of a dlistint_t linked list
  * @head: pointer to the head of the list
  * Return: sum of all the data, or 0 if the list is empty
  */
