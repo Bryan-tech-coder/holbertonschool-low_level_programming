a309247e96236808da947f138105330e17404ff7
Bryan <br1173626@gmail.com>
2025-11-28 13:44:07 -0400

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -7,30 +7,30 @@
  *
  * Return: 1 on success, -1 on failure.
  */
-int create_file(const char *filename, char *text_content)  
+int create_file(const char *filename, char *text_content)
 {
-    int fd, bytes_written, text_length = 0;
+	int fd, bytes_written, text_length = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[text_length] != '\0')
-            text_length++;
+	if (text_content != NULL)
+	{
+		while (text_content[text_length] != '\0')
+			text_length++;
 
-        bytes_written = write(fd, text_content, text_length);
-        if (bytes_written == -1 || bytes_written != text_length)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+		bytes_written = write(fd, text_content, text_length);
+		if (bytes_written == -1 || bytes_written != text_length)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-    close(fd);
-    return (1);
+	close(fd);
+	return (1);
 }
